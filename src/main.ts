24b924f9fd08a3b54834826e412157c32b7570e1
oayee <selass.selass.selass@gmail.com>
2019-09-02 17:18:06 +0300

    added dynamic port, changed scripts for heroku

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
