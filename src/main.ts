a26c93a5bc97a7eb7f4dd257b72d1dcb73dddd1f
oayee <selass.selass.selass@gmail.com>
2019-09-03 13:56:34 +0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 
