91d1b036c4ac1041ef61a8b3bd1e0cc58fbf879e
oayee <selass.selass.selass@gmail.com>
2019-09-03 13:56:54 +0300

    generated answers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DbModule } from './db/db.module';
+import { AnswersModule } from './answers/answers.module';
 
 @Module({
   imports: [
     AuthModule,
     UsersModule,
     DbModule,
+    AnswersModule,
   ],
   controllers: [AppController],
 })
