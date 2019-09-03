71661c79694c23668a991f852a75187f46335556
oayee <selass.selass.selass@gmail.com>
2019-09-03 16:53:05 +0300

    values made optional

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -8,11 +8,11 @@ export class DbService {
   private readonly connection = 'postgres://qxelholmaeucta:1e52fa294d8130cfdeb98c5619dfb8235280c683ef859f2d3090da2b555ddfb7@ec2-54-246-121-32.eu-west-1.compute.amazonaws.com:5432/d3h6k1kepkg9hd?ssl=true';
   private readonly db = this.pgp(this.connection);
 
-  async findOne(query: string, values: any[]) {
+  async findOne(query: string, values?: any[]) {
     return await this.db.one(query, values);
   }
 
-  async find(query: string, values: any[]) {
+  async find(query: string, values?: any[]) {
     return await this.db.any(query, values);
   }
 
