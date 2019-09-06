0c58ec0821d96a4adbef937986ee35dafcbd71d4
oayee <selass.selass.selass@gmail.com>
2019-09-06 15:58:47 +0300

    added quiz\question to history

--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -10,12 +10,15 @@ export class HistoryService {
     const query = `select mb.brand, m2.inventory_number, m2.plate_number,
     array_agg(
       json_build_object(
-        'answerId', a.id, 'answer', a.answer, 'dateCreated', a.date_created
+        'answerId', a.id, 'answer', a.answer, 'dateCreated', a.date_created,
+        'quiz', q.quiz, 'question', q.question
         )
     ) as "answers"
     from answers a
     join machines m2 on a.machine_id = m2.id
     join machine_brands mb on m2.brand_id = mb.id
+    join machine_types t on m2.type_id = t.id
+    join quizzes q on t.id = q.machine_type_id
     where mb.id = 1
     group by brand, inventory_number, plate_number`;
     return await this.db.findOne(query, [brandId]);
