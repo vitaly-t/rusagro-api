6a12be0cc6c52fcb8a15e178f5b40685fbfbd6c7
Barcius <molaanus@gmail.com>
2019-09-12 14:55:48 +0300

    analytics main view

--- src/analytics/analytics.service.ts
+++ src/analytics/analytics.service.ts
@@ -7,7 +7,22 @@ export class AnalyticsService {
   }
 
   findQuizzes() {
-    const query = ``;
+    const query = `
+    with selected as (select
+          q.id,
+          q.quiz,
+          q.description,
+          count(a.id)       as "quiz_count",
+          max(a.date_updated) as "latestUpdated"
+        from quizzes q
+          left join sss s on s.type_id = q.machine_type_id
+          left join answers a on a.sss_id = s.id
+        group by q.id, q.description)
+    select
+    selected.*,
+    date_created as "latestCreated"
+    from answers a
+    join selected on selected."latestUpdated" = a.date_updated;`;
     return this.db.find(query);
   }
 }
