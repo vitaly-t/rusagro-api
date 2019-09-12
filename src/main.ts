654eb0bb4d9027a72c7dc3a064df3cac655f45ec
oayee <selass.selass.selass@gmail.com>
2019-09-12 16:00:48 +0300

    added dev https

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { HttpExceptionFilter } from './core/exception/http-exception.filter';
 import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('C:\\Testing\\localhost+1-key.pem').toString(),
-    cert: fs.readFileSync('C:\\Testing\\localhost+1.pem').toString(),
-  };
+  const httpsOptions = { key: '', cert: '' };
+  if (process.env.NODE_DEV === 'true') {
+    httpsOptions.key = fs.readFileSync('C:\\Testing\\localhost+1-key.pem').toString();
+    httpsOptions.cert = fs.readFileSync('C:\\Testing\\localhost+1.pem').toString();
+  }
   const optsObj = process.env.NODE_DEV === 'true' ? { httpsOptions } : {};
   const app = await NestFactory.create(AppModule, optsObj);
   app.setGlobalPrefix('api');
