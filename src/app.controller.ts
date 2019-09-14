2ce5fd86b683444f1bdfb5172481e2dca8997ca8
oayee <selass.selass.selass@gmail.com>
2019-09-14 14:04:04 +0300

    chaged /ping response status to 202 (costyl')

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get('ping')
+  @HttpCode(202)
   async ping() {
     return { success: true };
   }
