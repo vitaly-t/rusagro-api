05d3f6f959eedd46f7a98c9f9ace059187612184
oayee <selass.selass.selass@gmail.com>
2019-09-14 11:56:42 +0300

    added /ping

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  @Get('ping')
+  async ping() {
+    return { success: true };
+  }
 }
